7a91a8ef2b9d99e6667f2cf31312fc676123bd2b
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-06-10 16:47:58 +0300

    food_page Wrap and List.genertor added

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,18 +1,27 @@
+import 'dart:ffi';
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/assets/color/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
-  const FoodPageBody({super.key});
+  const FoodPageBody({Key? key}) : super(key: key);
 
   @override
   State<FoodPageBody> createState() => _FoodPageBodyState();
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
+      color: Colors.redAccent,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position) {
           return _buildPageItem(position);
@@ -22,16 +31,77 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: const EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-        image: DecorationImage(
-          image: AssetImage("lib/assets/image/food1.png"),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven
+                ? const Color(0xFF69c5df)
+                : const Color(0xFF9294cc),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("lib/assets/image/food1.png"),
+            ),
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                          children: List.generate(5, (index) {
+                        return Icon(
+                          Icons.star,
+                          size: 20,
+                          color: AppColors.mainColor,
+                        );
+                      })),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(
+                        text: "4.5",
+                        color: AppColors.mainBlackColor,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(
+                        text: "1287",
+                        color: AppColors.mainBlackColor,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(
+                        text: "comments",
+                        color: AppColors.mainBlackColor,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
