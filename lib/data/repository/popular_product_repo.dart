ca8dcdbf597f0b192767ae06af953599d9a2219e
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-06-18 21:07:01 +0300

    editing

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -7,7 +7,7 @@ class PopularProductRepo extends GetxService {
   PopularProductRepo({required this.apiClient});
   Future<Response> getPopularProductlist() async {
     
-    return await apiClient.getData("http://mvs.blsmeiyu.com/api/v1/produtcs/popular");
+    return await apiClient.getData("http://mvs.blsmeiyu.com/api/v1/produtcs/popular.error");
 
   }
 }
